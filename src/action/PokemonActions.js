f694c60fafc09f324483a50131090f68ee8ab273
pratik <lazy.monk.13@gmail.com>
2016-09-01 11:25:49 -0700

    added thunk

--- src/action/PokemonActions.js
+++ src/action/PokemonActions.js
@@ -1,3 +1,21 @@
+import axios from 'axios';
+
+// synchronous action creator
+export function receivePokemon(pokemon) {
+	return {
+		type: 'RECEIVE_POKEMON',
+		payload: { pokemon }
+	}
+}
+
+// asynchronous action
 export function fetchPokemon() {
-	
+	return dispatch => {
+		axios.get('./api/pokemon')
+			.then(res => res.data)
+			.then(pokemon => {
+				dispatch(receivePokemon(pokemon));
+			})
+			.catch(console.error)
+	}
 }
